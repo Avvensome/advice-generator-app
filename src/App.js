ec62210c4626ec1fbb008269313eb4639d095887
Patryk <p.zawistowski190798@gmail.com>
2023-09-07 13:56:33 +0200

    set-up static desktop version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 function App() {
-	return <div className="App">Hello There</div>;
+	function MainContainer({ containerClass, children }) {
+		return <div className={containerClass}>{children}</div>;
+	}
+	function AdviceNumber() {
+		return <p className="advice-number">Advice #123</p>;
+	}
+	function ButtonSvg({ btnClass, imgSrc, imgAlt, imgClass }) {
+		return (
+			<button className={btnClass}>
+				<img className={imgClass} src={imgSrc} alt={imgAlt} />
+			</button>
+		);
+	}
+	function AdviceContainer({ children }) {
+		return <p className="advice">{children}</p>;
+	}
+	return (
+		<MainContainer containerClass={"app-main"}>
+			<AdviceNumber />
+			<AdviceContainer>
+				"It is easy to sit up and trake notice, what's difficult is
+				getting up and taking ation."
+			</AdviceContainer>
+			<img
+				src="/images/pattern-divider-desktop.svg"
+				alt="Pattern Divider"
+			/>
+			<ButtonSvg
+				btnClass={"dice-btn"}
+				imgSrc={"/images/icon-dice.svg"}
+				imgAlt={"Dice icon"}
+				imgClass={"dice"}
+			/>
+		</MainContainer>
+	);
 }
 
 export default App;
