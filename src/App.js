91b176a33f4636e02284672440d597acce99a51c
Patryk <p.zawistowski190798@gmail.com>
2023-09-08 20:28:19 +0200

    Code After Review

--- src/App.js
+++ src/App.js
@@ -1,69 +1,50 @@
 import { useEffect, useState } from "react";
 
 function App() {
-	const [advice, setAdvice] = useState("Your Test Advice");
-	const [adviceId, setAdviceId] = useState(0);
-	function MainContainer({ containerClass, children }) {
-		return <div className={containerClass}>{children}</div>;
-	}
-	function AdviceNumber() {
-		return <p className="advice-number">Advice {`#${adviceId}`}</p>;
-	}
-	function ButtonSvg({ btnClass, imgSrc, imgAlt, imgClass, onClick }) {
-		return (
-			<button className={btnClass} onClick={onClick}>
-				<img className={imgClass} src={imgSrc} alt={imgAlt} />
-			</button>
-		);
-	}
-	function AdviceContainer({ children }) {
-		return <p className="advice">{children}</p>;
-	}
-	function AdviceDivider() {
-		return (
-			<>
-				{window.innerWidth > 375 ? (
-					<img
-						src="/images/pattern-divider-desktop.svg"
-						alt="Pattern Divider"
-					/>
-				) : (
-					<img
-						src="/images/pattern-divider-mobile.svg"
-						alt="Pattern Divider"
-					/>
-				)}
-			</>
-		);
-	}
+	const [adviceObj, setAdviceObj] = useState({
+		adviceText: "Test Advice",
+		id: 0,
+	});
+
 	async function getAdvice() {
 		const rest = await fetch("https://api.adviceslip.com/advice");
 		const data = await rest.json();
 		const {
 			slip: { id, advice },
 		} = data;
-		setAdvice(advice);
-		setAdviceId(id);
+		setAdviceObj({ ...adviceObj, adviceText: advice, id: id });
 	}
 	useEffect(function () {
 		getAdvice();
-		console.log();
 	}, []);
 	return (
-		<MainContainer containerClass={"app-main"}>
-			<AdviceNumber />
-			<AdviceContainer>{advice}</AdviceContainer>
-			<AdviceDivider />
-			<ButtonSvg
+		<div className="app-main">
+			<p className="advice-number">Advice #{adviceObj.id}</p>
+			<p className="advice">{adviceObj.adviceText}</p>
+			{window.innerWidth > 375 ? (
+				<img
+					src="/images/pattern-divider-desktop.svg"
+					alt="Pattern Divider"
+				/>
+			) : (
+				<img
+					src="/images/pattern-divider-mobile.svg"
+					alt="Pattern Divider"
+				/>
+			)}
+			<button
+				aria-label={"Get another Advice"}
+				className={"dice-btn"}
 				onClick={getAdvice}
-				btnClass={"dice-btn"}
-				imgSrc={"/images/icon-dice.svg"}
-				imgAlt={"Dice icon"}
-				imgClass={"dice"}
-			/>
-		</MainContainer>
+			>
+				<img
+					src={"/images/icon-dice.svg"}
+					alt={"Dice icon"}
+					className={"dice"}
+				/>
+			</button>
+		</div>
 	);
 }
 
 export default App;
-// W useEfet stworzyć dwa useState ktore bea przechowywać porade i id, a potem uzyc tego w komponentach
